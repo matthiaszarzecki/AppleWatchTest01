6528c523a33d042219b7e43ac47fae5cbd5083a3
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2024-07-02 08:01:21 +0200

    Add basic timer code

--- AppleWatchApp/AppleWatchApp Watch App/ContentView.swift
+++ AppleWatchApp/AppleWatchApp Watch App/ContentView.swift
@@ -14,9 +14,82 @@ struct ContentView: View {
         .imageScale(.large)
         .foregroundStyle(.tint)
 
-      Text("Hello, world!")
+      Button(
+        action: {},
+        label: {
+          Text("Button!")
+        }
+      )
     }
     .padding()
+  }
+
+
+  @State var secondsElapsed = 0
+  var timer = Timer()
+  @State var isTimerRunning = false
+  @State var resumeTapped = false
+
+  /*override func viewDidLoad() {
+      pauseButton.isEnabled = false
+      super.viewDidLoad()
+  }
+  */
+
+  func runTimer() {
+    /*timer = Timer.scheduledTimer(
+     timeInterval: 1,
+     target: self,
+     selector: (#selector(ViewController.updateTimer)),
+     userInfo: nil,
+     repeats: true
+     )
+     pauseButton.isEnabled = true*/
+  }
+
+  func updateTimer() {
+    if secondsElapsed < 1 {
+      timer.invalidate()
+    } else {
+      secondsElapsed += 1
+      //updateDisplay()
+    }
+  }
+
+  func pauseButtonClicked() {
+    if self.resumeTapped == false {
+      timer.invalidate()
+      //self.resumeTapped = true
+      //self.pauseButton.setTitle("Resume", for: .normal)
+    } else {
+      //runTimer()
+      //self.resumeTapped = false
+      //self.pauseButton.setTitle("Pause", for: .normal)
+    }
+  }
+
+  func startButtonClicked() {
+    if !isTimerRunning {
+      //runTimer()
+      isTimerRunning = true
+      //self.startButton.isEnabled = false
+    }
+  }
+
+  func reset() {
+    timer.invalidate()
+    secondsElapsed = 0
+    //updateDisplay()
+    isTimerRunning = false
+    //pauseButton.isEnabled = false
+    //startButton.isEnabled = true
+  }
+
+  private func timeString(time: TimeInterval) -> String {
+    let hours = Int(time) / 3600
+    let minutes = Int(time) / 60 % 60
+    let seconds = Int(time) % 60
+    return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
   }
 }
 
