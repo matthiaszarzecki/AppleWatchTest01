06cd3421407c9643944cb20ebf231fceb6adffa8
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2024-07-18 17:47:28 +0200

    Add runstate Enum

---
+++ AppleWatchApp/AppleWatchApp Watch App/Models/RunState.swift
@@ -0,0 +1,12 @@
+//
+//  RunState.swift
+//  AppleWatchApp Watch App
+//
+//  Created by Matthias Zarzecki on 18.07.24.
+//
+
+enum RunState {
+  case hasNotStarted
+  case inProgress
+  case finished
+}
--- AppleWatchApp/AppleWatchApp Watch App/Views/RunView.swift
+++ AppleWatchApp/AppleWatchApp Watch App/Views/RunView.swift
@@ -11,24 +11,23 @@ import SwiftUI
 struct RunView: View {
   @State private var timeRemaining = 90
   @State private var currentPeriodIndex = 0
-  @State private var vibrated = false
-
-  let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+  @State private var hasVibrated = false
   @State private var currentSession = CurrentSession()
-  @State private var hasStarted = false
+  @State private var runState: RunState = .hasNotStarted
+  @State private var currentDate: Date = .now
 
-  @State private var currentDate = Date.now
+  private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
   var body: some View {
     VStack {
       HStack {
         Image(systemName: "timer")
-          .imageScale(.large)
+          .font(.system(size: 42))
           .foregroundStyle(.green)
 
         VStack {
           Text("Time Left: \(timeRemaining)s")
-          Text("Interval: \(currentPeriodIndex)/\(currentSession.trainingDay.periods.count)")
+          Text("Interval: \(currentPeriodIndex + 1)/\(currentSession.trainingDay.periods.count)")
 
           if let period = currentSession.trainingDay.periods[safe: currentPeriodIndex] {
             Text(period.description)
@@ -36,105 +35,83 @@ struct RunView: View {
           }
         }
       }
+
       HStack {
-        // Reset Button
-        Button(
-          action: {
-            //timer = Timer.publish(every: 1, on: .main, in: .common)
-            //timer?.cancel()
-
-            hasStarted = false
-            currentPeriodIndex = 0
-            if let period = currentSession.trainingDay.periods[safe: currentPeriodIndex] {
-              timeRemaining = Int(period.duration)
-            }
-
-            vibrated = false
-            currentSession.dataPoints = []
-            WKInterfaceDevice.current().play(.failure)
-          },
-          label: {
-            Text("Reset")
-          }
-        )
-
-        // Start / Skip button
-        Button(
-          action: {
-            if currentPeriodIndex == 0 && !hasStarted {
-              hasStarted = true
-              //assignTimer()
-            } else {
-              currentPeriodIndex += 1
-            }
-
-            if let period = currentSession.trainingDay.periods[safe: currentPeriodIndex] {
-              timeRemaining = Int(period.duration)
-            }
-
-            vibrated = false
-            WKInterfaceDevice.current().play(.notification)
-          },
-          label: {
-            Text(currentPeriodIndex == 0 && !hasStarted ? "Start" : "Skip")
-          }
-        )
+        resetButton
+        startSkipButton
       }
 
-      Text("DataPoints: \(currentSession.dataPoints.count)")
+      //Text("DataPoints: \(currentSession.dataPoints.count)")
     }
     .padding()
     .onReceive(timer) { value in
-      print(value)
-      currentDate = value
-
-      if hasStarted {
-        if timeRemaining > 0 {
-          // Regular Interval - Count Down
-          vibrated = false
-          timeRemaining -= 1
-          currentSession.dataPoints.append(Date())
-        }
-        if timeRemaining <= 0 && !vibrated {
-          // Timer Reached Zero - Reset
-          WKInterfaceDevice.current().play(.notification)
-          vibrated = true
-
-          currentPeriodIndex += 1
-          if let period = currentSession.trainingDay.periods[safe: currentPeriodIndex] {
-            timeRemaining = Int(period.duration)
-          }
-        }
-      }
+      update(value)
     }
   }
 
-  private func assignTimer() {
-    /*timer = Timer
-      .publish(every: 1, on: .main, in: .common)
-      .autoconnect()
-      .sink { value in
-        print(value)
-
-        if hasStarted {
-          if timeRemaining > 0 {
-            // Regular Interval - Count Down
-            vibrated = false
-            timeRemaining -= 1
-            currentSession.dataPoints.append(Date())
-          }
-          if timeRemaining <= 0 && !vibrated {
-            // Timer Reached Zero - Reset
-            WKInterfaceDevice.current().play(.notification)
-            vibrated = true
-
-            currentPeriodIndex += 1
-            if let period = currentSession.trainingDay.periods[safe: currentPeriodIndex] {
-              timeRemaining = Int(period.duration)
-            }
-          }
+  private var resetButton: some View {
+    Button(
+      action: {
+        runState = .hasNotStarted
+        currentPeriodIndex = 0
+        if let period = currentSession.trainingDay.periods[safe: currentPeriodIndex] {
+          timeRemaining = Int(period.duration)
         }
-      }*/
+
+        hasVibrated = false
+        currentSession.dataPoints = []
+        WKInterfaceDevice.current().play(.failure)
+      },
+      label: {
+        Text("Reset")
+      }
+    )
+  }
+
+  private var startSkipButton: some View {
+    Button(
+      action: {
+        if currentPeriodIndex == 0 && runState == .hasNotStarted {
+          runState = .inProgress
+        } else {
+          currentPeriodIndex += 1
+        }
+
+        if let period = currentSession.trainingDay.periods[safe: currentPeriodIndex] {
+          timeRemaining = Int(period.duration)
+        }
+
+        hasVibrated = false
+        WKInterfaceDevice.current().play(.notification)
+      },
+      label: {
+        Text(currentPeriodIndex == 0 && runState == .hasNotStarted ? "Start" : "Skip")
+      }
+    )
+  }
+
+  private func update(_ date: Date) {
+    print(date)
+    currentDate = date
+
+    if runState == .inProgress {
+      if timeRemaining > 0 {
+        // Regular Interval - Count Down
+        hasVibrated = false
+        timeRemaining -= 1
+        currentSession.dataPoints.append(Date())
+      }
+      if timeRemaining <= 0 && !hasVibrated {
+        // Timer Reached Zero - Reset
+        WKInterfaceDevice.current().play(.notification)
+        hasVibrated = true
+
+        currentPeriodIndex += 1
+        if let period = currentSession.trainingDay.periods[safe: currentPeriodIndex] {
+          timeRemaining = Int(period.duration)
+        }
+      }
+    }
   }
 }
 
