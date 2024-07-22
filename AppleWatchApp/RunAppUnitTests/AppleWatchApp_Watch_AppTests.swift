a213cf768fd93e7ab1df88a01e37fed8ecade33f
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2024-07-22 11:29:43 +0200

    Update testing

---
+++ AppleWatchApp/RunAppUnitTests/AppleWatchApp_Watch_AppTests.swift
@@ -0,0 +1,15 @@
+//
+//  AppleWatchApp_Watch_AppTests.swift
+//  AppleWatchApp Watch AppTests
+//
+//  Created by Matthias Zarzecki on 30.06.24.
+//
+
+import XCTest
+@testable import RunApp
+
+final class AppleWatchApp_Watch_AppTests: XCTestCase {
+  func testExample() {
+    XCTAssertTrue(true)
+  }
+}
